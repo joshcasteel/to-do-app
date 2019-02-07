8d30f5ee9554d91d720dc2d861ada3620288c8b5
joshcasteel <joshcasteelz@gmail.com>
2019-02-07 12:06:15 -0500

    added delete to todo items

--- src/App.js
+++ src/App.js
@@ -5,14 +5,22 @@ class App extends Component {
   state = {
     todos: [
       { id: 1, content: "buy some milk" },
-      { id: 2, conent: "play mario kart" }
+      { id: 2, content: "play mario kart" }
     ]
+  };
+  deleteTodo = id => {
+    const todos = this.state.todos.filter(todo => {
+      return todo.id !== id;
+    });
+    this.setState({
+      todos
+    });
   };
   render() {
     return (
-      <div className="App">
+      <div className="todo-app container">
         <h1 className="center blue-text">Todo's</h1>
-        <Todos todos={this.state.todos} />
+        <Todos todos={this.state.todos} deleteTodo={this.deleteTodo} />
       </div>
     );
   }
